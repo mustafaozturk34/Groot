786d1eab6298a67710260451a6bc91412e4fd1d1
Muhammet Mustafa ÖZTÜRK <77894227+mustafaozturk34@users.noreply.github.com>
2021-12-10 13:14:20 +0300

    adding entityframework and db connection

---
+++ Groot.DB/Entities/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Groot.DB.Entities
+{
+    public partial class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime Idate { get; set; }
+        public DateTime? Udate { get; set; }
+        public int Iuser { get; set; }
+        public int? Uuser { get; set; }
+
+        public virtual User IuserNavigation { get; set; }
+    }
+}
---
+++ Groot.DB/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Groot.DB.Entities
+{
+    public partial class User
+    {
+        public User()
+        {
+            Category = new HashSet<Category>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime Idatetime { get; set; }
+        public DateTime? UdateTime { get; set; }
+        public int Iuser { get; set; }
+        public int? Uuser { get; set; }
+
+        public virtual ICollection<Category> Category { get; set; }
+    }
+}
